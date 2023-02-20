61da06e3552155bf3c8011f60be7b8ae2a8d23ae
Agostinho Massango <massango541@gmail.com>
2023-02-20 14:40:41 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 
+
 /**
  * main - prints "Programming is like building a multilingual
  * puzzle, followed by a newline."
@@ -7,6 +8,7 @@
  */
 
 int main (void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
